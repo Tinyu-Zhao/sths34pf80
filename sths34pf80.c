2f1e2f05d9e799edc449ed7d4792cea4cd52ade3
Tinyu <Tinyu.Zhao@gmail.com>
2024-09-06 13:37:29 +0800

    ulptmos

--- sths34pf80.c
+++ sths34pf80.c
@@ -1,49 +1,17 @@
-/**
-  ******************************************************************************
-  * @file           : sths34pf80.c
-  * @author         : Mauricio Barroso Benavides
-  * @date           : Jun 18, 2024
-  * @brief          : todo: write brief
-  ******************************************************************************
-  * @attention
-  *
-  * MIT License
-  *
-  * Copyright (c) 2024 Mauricio Barroso Benavides
-  *
-  * Permission is hereby granted, free of charge, to any person obtaining a copy
-  * of this software and associated documentation files (the "Software"), to
-  * deal in the Software without restriction, including without limitation the
-  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-  * sell copies of the Software, and to permit persons to whom the Software is
-  * furnished to do so, subject to the following conditions:
-  *
-  * The above copyright notice and this permission notice shall be included in
-  * all copies or substantial portions of the Software.
-  * 
-  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-  * IN THE SOFTWARE.
-  *
-  ******************************************************************************
-  */
-
 /* Includes ------------------------------------------------------------------*/
-#include "sths34pf80.h"
+#include "lpsths34pf80.h"
 
 #include "esp_err.h"
-#include "esp_log.h"
-#include "esp_timer.h"
+#include "ulp_lp_core_print.h"
 
 /* Private macros ------------------------------------------------------------*/
-#define NOP() asm volatile ("nop")
+#define NOP() asm volatile("nop")
 
-#define STHS34PF80_I2C_BUFFER_LEN_MAX	(256)
-#define STHS34PF80_BOOT_TIME_MS			(10)
+#define STHS34PF80_I2C_BUFFER_LEN_MAX (256)
+#define STHS34PF80_BOOT_TIME_MS       (10)
+#define LP_I2C_TRANS_TIMEOUT_CYCLES   5000
+#define LP_I2C_TRANS_WAIT_FOREVER     -1
+#define STHS34PF80_I2C_ADDR           0x5A
 
 /* External variables --------------------------------------------------------*/
 
@@ -51,7 +19,7 @@
 
 /* Private variables ---------------------------------------------------------*/
 static const char *TAG = "sths34pf80";
-
+i2c_port_t lp_i2c_num;
 /* Private function prototypes -----------------------------------------------*/
 /**
  * @brief Function that implements the default I2C read transaction
@@ -63,8 +31,7 @@ static const char *TAG = "sths34pf80";
  *
  * @return 0 if successful, non-zero otherwise
  */
-static int32_t i2c_read(void *handle, uint8_t reg_addr, uint8_t *reg_data,
-		uint16_t data_len);
+static esp_err_t i2c_read(void *handle, uint8_t reg_addr, uint8_t *reg_data, uint16_t data_len);
 /**
  * @brief Function that implements the default I2C write transaction
  *
@@ -75,8 +42,7 @@ static int32_t i2c_read(void *handle, uint8_t reg_addr, uint8_t *reg_data,
  *
  * @return 0 if successful, non-zero otherwise
  */
-static int32_t i2c_write(void *handle, uint8_t reg_addr, const uint8_t *reg_data,
-		uint16_t data_len);
+static esp_err_t i2c_write(void *handle, uint8_t reg_addr, const uint8_t *reg_data, uint16_t data_len);
 /**
  * @brief Function that implements a micro seconds delay
  *
@@ -88,150 +54,145 @@ static void delay_ms(uint32_t period_ms);
 /**
  * @brief Function to initialize a STHS34PF80 instance
  */
-esp_err_t sths34pf80_init(sths34pf80_t *const me,
-		i2c_master_bus_handle_t i2c_bus_handle, uint8_t dev_addr)
+esp_err_t sths34pf80_init(lpsths34pf80_t *const me, i2c_port_t i2c_num, uint8_t dev_addr)
 {
-	ESP_LOGI(TAG, "Initializing STHS34PF80 instance...");
+    lp_core_printf("Initializing STHS34PF80 instance...\n");
 
-	/* Variable to return error code */
-	esp_err_t ret = ESP_OK;
+    /* Add device to I2C bus */
 
-	/* Add device to I2C bus */
-	i2c_device_config_t i2c_dev_conf = {
-			.scl_speed_hz = 400000,
-			.device_address = dev_addr >> 1
-	};
+    me->i2c_dev = NULL;
 
-	if (i2c_master_bus_add_device(i2c_bus_handle, &i2c_dev_conf, &me->i2c_dev) != ESP_OK) {
-		ESP_LOGE(TAG, "Failed to add device to I2C bus");
-		return ret;
-	}
+    /* Fill driver interface */
+    me->stmdev_ctx.write_reg = i2c_write;
+    me->stmdev_ctx.read_reg  = i2c_read;
+    me->stmdev_ctx.handle    = me->i2c_dev;
+    me->stmdev_ctx.mdelay    = delay_ms;
 
-	/* Fill driver interface */
-	me->stmdev_ctx.write_reg = i2c_write;
-	me->stmdev_ctx.read_reg = i2c_read;
-	me->stmdev_ctx.handle = me->i2c_dev;
-	me->stmdev_ctx.mdelay = delay_ms;
+    lp_i2c_num = i2c_num;
 
-	/* Wait for sensor boot time */
-	delay_ms(STHS34PF80_BOOT_TIME_MS);
+    /* Wait for sensor boot time */
+    delay_ms(STHS34PF80_BOOT_TIME_MS);
 
-	/* Check device ID */
-	uint8_t whoami;
-	sths34pf80_device_id_get(&me->stmdev_ctx, &whoami);
+    /* Check device ID */
+    uint8_t whoami;
+    sths34pf80_device_id_get(&me->stmdev_ctx, &whoami);
 
-	if (whoami != STHS34PF80_ID) {
-		ESP_LOGE(TAG, "Invalid device ID");
-		return ESP_FAIL;
-	}
+    if (whoami != STHS34PF80_ID) {
+        lp_core_printf("Invalid device ID\n");
+        return ESP_FAIL;
+    } else {
+        lp_core_printf("Device ID: %02X\n", whoami);
+    }
 
-	/* Set averages (AVG_TAMB = 8, AVG_TMOS = 32) */
-	sths34pf80_avg_tobject_num_set(&me->stmdev_ctx, STHS34PF80_AVG_TMOS_32);
-	sths34pf80_avg_tambient_num_set(&me->stmdev_ctx, STHS34PF80_AVG_T_8);
+    /* Set averages (AVG_TAMB = 8, AVG_TMOS = 32) */
+    sths34pf80_avg_tobject_num_t testData;
+    sths34pf80_avg_tobject_num_get(&me->stmdev_ctx, &testData);
+    lp_core_printf("testData: %d\n", testData);
+    int32_t avgErr = sths34pf80_avg_tobject_num_set(&me->stmdev_ctx, STHS34PF80_AVG_TMOS_32);
+    sths34pf80_avg_tobject_num_get(&me->stmdev_ctx, &testData);
+    lp_core_printf("testData: %d\n", testData);
 
-	/* Read filters */
-	sths34pf80_lpf_bandwidth_t lpf_m, lpf_p, lpf_p_m, lpf_a_t;
-	sths34pf80_lpf_m_bandwidth_get(&me->stmdev_ctx, &lpf_m);
-	sths34pf80_lpf_p_bandwidth_get(&me->stmdev_ctx, &lpf_p);
-	sths34pf80_lpf_p_m_bandwidth_get(&me->stmdev_ctx, &lpf_p_m);
-	sths34pf80_lpf_a_t_bandwidth_get(&me->stmdev_ctx, &lpf_a_t);
+    sths34pf80_avg_tambient_num_t testData2;
+    sths34pf80_avg_tambient_num_get(&me->stmdev_ctx, &testData2);
+    lp_core_printf("testData2: %d\n", testData2);
 
-	ESP_LOGI(TAG, "lpf_m: %02d, lpf_p: %02d, lpf_p_m: %02d, lpf_a_t: %02d", lpf_m, lpf_p, lpf_p_m, lpf_a_t);
+    int32_t tAmbErr = sths34pf80_avg_tambient_num_set(&me->stmdev_ctx, STHS34PF80_AVG_T_8);
+    sths34pf80_avg_tambient_num_get(&me->stmdev_ctx, &testData2);
+    lp_core_printf("testData2: %d\n", testData2);
 
-	/* Set BDU */
-	sths34pf80_block_data_update_set(&me->stmdev_ctx, 1);
+    lp_core_printf("avgErr: %d, tAmbErr: %d\n", avgErr, tAmbErr);
+    return ESP_OK;
 
-	/* Return ESP_OK */
-	return ret;
+    /* Read filters */
+    sths34pf80_lpf_bandwidth_t lpf_m, lpf_p, lpf_p_m, lpf_a_t;
+    sths34pf80_lpf_m_bandwidth_get(&me->stmdev_ctx, &lpf_m);
+    sths34pf80_lpf_p_bandwidth_get(&me->stmdev_ctx, &lpf_p);
+    sths34pf80_lpf_p_m_bandwidth_get(&me->stmdev_ctx, &lpf_p_m);
+    sths34pf80_lpf_a_t_bandwidth_get(&me->stmdev_ctx, &lpf_a_t);
+
+    lp_core_printf("lpf_m: %02d, lpf_p: %02d, lpf_p_m: %02d, lpf_a_t: %02d\n", lpf_m, lpf_p, lpf_p_m, lpf_a_t);
+
+    /* Set BDU */
+    int32_t blockErr = sths34pf80_block_data_update_set(&me->stmdev_ctx, 1);
+    lp_core_printf("blockErr: %d\n", blockErr);
+    ulp_lp_core_delay_us(10);
+
+    // 这一行有问题
+    int32_t odrErr = sths34pf80_odr_set(&me->stmdev_ctx, STHS34PF80_ODR_AT_1Hz);
+
+    lp_core_printf("blockErr: %d, odrErr: %d\n", blockErr, odrErr);
+    /////////int32_t odrErr = sths34pf80_set_odr(&me->stmdev_ctx, STHS34PF80_ODR_AT_1Hz);
+    if (avgErr != 0) {
+        return avgErr;
+    } else if (tAmbErr != 0) {
+        return tAmbErr;
+    } else if (blockErr != 0) {
+        return blockErr;
+    } else if (odrErr != 0) {
+        return odrErr;
+    }
+    lp_core_printf(("avgErr: %d, tAmbErr: %d, blockErr: %d, odrErr: %d\n"), avgErr, tAmbErr, blockErr, odrErr);
+
+    // If no errors, return 0
 }
 
 /**
  * @brief Function to set the ODR (Output Data Rate)
  */
-esp_err_t sths34pf80_set_odr(sths34pf80_t *const me, sths34pf80_odr_t val)
+esp_err_t sths34pf80_set_odr(lpsths34pf80_t *const me, sths34pf80_odr_t val)
 {
-	/* Variable to return error code */
-	esp_err_t ret = ESP_OK;
+    /* Variable to return error code */
+    esp_err_t ret = ESP_OK;
 
-	/**/
-	if (sths34pf80_odr_set(&me->stmdev_ctx, val) != 0) {
-		ESP_LOGE(TAG, "Failed to set ODR");
-		return ESP_FAIL;
-	}
+    /**/
+    if (sths34pf80_odr_set(&me->stmdev_ctx, val) != 0) {
+        lp_core_printf("Failed to set ODR\n");
+        return ESP_FAIL;
+    }
 
-	/* Return ESP_OK */
-	return ret;
+    /* Return ESP_OK */
+    return ret;
 }
 
-/* Private function definitions ----------------------------------------------*/
-/**
- * @brief Function that implements the default I2C read transaction
- */
-static int32_t i2c_read(void *handle, uint8_t reg_addr, uint8_t *reg_data,
-		uint16_t data_len)
+static esp_err_t i2c_read(void *handle, uint8_t reg_addr, uint8_t *reg_data, uint16_t data_len)
 {
-	i2c_master_dev_handle_t i2c_dev = (i2c_master_dev_handle_t)handle;
+    // lp_core_printf("lp_i2c_num: %d, reg_addr: %d, data_len: %d\n", lp_i2c_num, reg_addr, data_len);
+    uint8_t write_data[1];
+    write_data[0] = reg_addr;
+    // lp_core_printf("submem_addr: %d\n", write_data[0]);
+    esp_err_t ret = lp_core_i2c_master_write_read_device(lp_i2c_num, STHS34PF80_I2C_ADDR, write_data, 1, reg_data,
+                                                         data_len, LP_I2C_TRANS_TIMEOUT_CYCLES);
 
-	if (i2c_master_transmit_receive(i2c_dev, &reg_addr, 1, reg_data, data_len,
-			-1) != ESP_OK) {
-		return -1;
-	}
-
-	return 0;
+    // lp_core_printf("ret: %d\n", ret);
+    return ret;
 }
 
-/**
- * @brief Function that implements the default I2C write transaction
- */
-static int32_t i2c_write(void *handle, uint8_t reg_addr, const uint8_t *reg_data,
-		uint16_t data_len)
+// static esp_err_t i2c_write(void *handle, uint8_t reg_addr, const uint8_t *reg_data, uint16_t data_len)
+// {
+//     lp_core_printf("lp_i2c_num: %d, reg_addr: %d, data_len: %d\n", lp_i2c_num, reg_addr, data_len);
+//     return lp_core_i2c_master_write_to_device(lp_i2c_num, reg_addr, reg_data, data_len, LP_I2C_TRANS_TIMEOUT_CYCLES);
+// }
+
+static esp_err_t i2c_write(void *handle, uint8_t reg_addr, const uint8_t *reg_data, uint16_t data_len)
 {
-	i2c_master_dev_handle_t i2c_dev = (i2c_master_dev_handle_t)handle;
-
-	uint8_t buffer[STHS34PF80_I2C_BUFFER_LEN_MAX] = {0};
-	uint8_t addr_len = sizeof(reg_addr);
-
-	/* Copy the register address to buffer */
-	for (uint8_t i = 0; i < addr_len; i++) {
-		buffer[i] = (reg_addr & (0xFF << ((addr_len - 1 - i) * 8)))
-				>> ((addr_len - 1 - i) * 8);
-	}
-
-	/* Copy the data to buffer */
-	for (uint8_t i = 0; i < data_len; i++) {
-		buffer[i + addr_len] = reg_data[i];
-	}
-
-	/* Transmit buffer */
-	if (i2c_master_transmit(i2c_dev, buffer, addr_len + data_len, -1) != ESP_OK) {
-		return -1;
-	}
-
-	return 0;
+    lp_core_printf("reg_addr: %d, reg_data:%d, data_len: %d\n", reg_addr, *reg_data, data_len);
+    uint8_t buffer[data_len + 1];
+    buffer[0] = reg_addr;
+    memcpy(&buffer[1], reg_data, data_len);
+    lp_core_printf("buffer[0]: %d buffer[1]:%d\n", buffer[0], buffer[1]);
+    esp_err_t ret = lp_core_i2c_master_write_to_device(lp_i2c_num, STHS34PF80_I2C_ADDR, buffer, sizeof(buffer),
+                                                       LP_I2C_TRANS_WAIT_FOREVER);
+    lp_core_printf("ret: %d\n", ret);
+    return ret;
 }
-
 /**
  * @brief Function that implements a micro seconds delay
  */
 static void delay_ms(uint32_t period_ms)
 {
-	uint32_t period_us = period_ms * 1000;
+    uint32_t period_us = period_ms * 1000;
 
-	uint64_t m = (uint64_t)esp_timer_get_time();
-
-	if (period_us) {
-		uint64_t e = (m + period_us);
-
-		if (m > e) { /* overflow */
-			while ((uint64_t)esp_timer_get_time() > e) {
-				NOP();
-			}
-		}
-
-		while ((uint64_t)esp_timer_get_time() < e) {
-			NOP();
-		}
-	}
+    ulp_lp_core_delay_us(period_us);
 }
 
 /***************************** END OF FILE ************************************/
